5b711b22eab373c341dd68fe15f4d6a675555da3
Unang Warsodi <unang.w@bts.id>
2025-04-29 14:16:15 +0700

    initial commit

---
+++ News-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News-App
+//
+//  Created by BTS.id on 29/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
